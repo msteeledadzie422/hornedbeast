d15ca918d8900ae48520fc742d5220407c978dad
Mandela Steele-Dadzie <msteeledadzie@gmail.com>
2022-07-31 14:23:19 -0600

    added code to make the page interactive

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -2,6 +2,10 @@ import { Component } from "react";
 import list from '../data.json';
 import HornedBeast from './HornedBeast.js';
 
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 class Main extends Component {
     constructor() {
         super();
@@ -11,15 +15,19 @@ class Main extends Component {
     }
     render() {
         return (
-            <div id='main'>
-                {this.state.beastList.map(element =>
-                    <HornedBeast
-                        title={element.title}
-                        image_url={element.image_url}
-                        description={element.description} />
+            <Container fluid>
+                <Row xs={1} sm={2}md={3} lg={4} xl={5}>
+                    {this.state.beastList.map(element =>
+                        <Col>
+                            <HornedBeast
+                                title={element.title}
+                                image_url={element.image_url}
+                                description={element.description} />
+                        </Col>
                     )}
-            </div>
-        )
+                </Row>
+            </Container>
+        );
     }
 }
 
