14535a799d1472589d9a4577b629f4fada282564
Mandela Steele-Dadzie <msteeledadzie@gmail.com>
2022-08-02 16:47:50 -0600

    removing useless constructor from selectedbeast file

--- src/Components/SelectedBeast.js
+++ src/Components/SelectedBeast.js
@@ -2,9 +2,6 @@ import { Component } from 'react';
 import Modal from 'react-bootstrap/Modal';
 
 class SelectedBeast extends Component {
-    constructor(props) {
-        super(props)
-    }
 
     render() {
         return (
